c9727b0401bdef3c077a4464ecc81ab6e5cc09c1
Kevin McKelvin <kmckelvin@gmail.com>
2015-12-13 15:26:11 +0200

    Split next instruction lookup to a separate method in the run loop

--- lib/elevator/run_loop.rb
+++ lib/elevator/run_loop.rb
@@ -1,12 +1,17 @@
 module Elevator
   class RunLoop
     def tick(shaft_state)
+      next_instruction = next_instruction(shaft_state)
+
       next_shaft_state = shaft_state.dup
-      next_instruction = InstructionResolver.new.next_instruction(next_shaft_state)
       next_step = next_instruction.next_step(next_shaft_state)
       next_step.apply(next_shaft_state)
       remove_fulfilled_instructions(shaft_state)
       next_shaft_state
+    end
+
+    def next_instruction(shaft_state)
+      InstructionResolver.new.next_instruction(shaft_state)
     end
 
     private
